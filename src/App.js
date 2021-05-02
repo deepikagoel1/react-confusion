4ca4e495cddf815ab60ef55579d6ab975ff29cdc
unknown <goeld9911@gmail.com>
2021-05-02 21:50:34 +0530

    Configure Bootstrap, CSS, Reactstrap which is React Based re-implementation of Component Element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar } from 'reactstrap'
+import { Navbar, NavbarBrand } from 'reactstrap'
 import './App.css';
 
 
@@ -8,15 +8,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React
-                </a>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
                 
             </div>
         );
