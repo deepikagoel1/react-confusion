bedcdc90d78722b826ed705fcc6c2ba863f63b8b
unknown <goeld9911@gmail.com>
2021-05-02 21:05:14 +0530

    Original file changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar } from 'reactstrap'
 import './App.css';
 
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React
+                </a>
+                
+            </div>
+        );
+    }
+}
+/*
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +44,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
